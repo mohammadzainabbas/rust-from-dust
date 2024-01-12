c930cdb9df4e7a718a1fbf230c93a2eb76fddd8d
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-12 20:16:13 +0100

    🧱 Laid groundwork for new requirement

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -2,7 +2,7 @@ use std::io;
 use rand::Rng;
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
+    println!("Please input your guess:");
     
     let secret_number = rand::thread_rng().gen_range(1..101);
     
