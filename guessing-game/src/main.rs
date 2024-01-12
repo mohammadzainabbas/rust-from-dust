859fbd83777fb430baff70ddb727572ceafaf904
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-12 20:14:48 +0100

    📚 Updated code

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -5,7 +5,8 @@ fn main() {
     println!("Please input your guess.");
 
     let secret_number = rand::thread_rng().gen_range(1..101);
-    
+    println!("Your secret number is {}", secret_number);
+
     let mut guess = String::new();
 
     io::stdin().read_line(&mut guess).expect("Unable to read line from stdin");
