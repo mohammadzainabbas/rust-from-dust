907d954f669858b30dc56e64c7a38ceeab258f09
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-12 20:55:22 +0100

    🚸 Improved new features

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -5,15 +5,17 @@ fn main() {
     println!("Guess the number!");
 
     let name: String = Input::with_theme(&ColorfulTheme::default()).with_prompt("Enter your name: ").interact_text().unwrap();
+    let guess: String = Input::with_theme(&ColorfulTheme::default()).with_prompt("Guess a number: ").interact_text().unwrap();
+
 
     println!("Your name is {}", name);
 
-    println!("Please input your guess:");
+    // println!("Please input your guess:");
 
     let secret_number = rand::thread_rng().gen_range(1..101);
     
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Unable to read line from stdin");
+    // let mut guess = String::new();
+    // io::stdin().read_line(&mut guess).expect("Unable to read line from stdin");
     
     println!("Your secret number is {}", secret_number);
     println!("Your guessed value is: {}", guess);
