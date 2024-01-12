b251f9772160497864637225e18fd71f336de09a
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-12 20:12:22 +0100

    🌟 Added a module

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -2,9 +2,9 @@ use std::io;
 use rand::Rng;
 fn main() {
     println!("Guess the number!");
-
     println!("Please input your guess.");
 
+    let secret_number = rand::Rng::
     let mut guess = String::new();
 
     io::stdin().read_line(&mut guess).expect("Unable to read line from stdin");
