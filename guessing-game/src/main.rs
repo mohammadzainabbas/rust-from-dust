a68022b84fbdf9e7f0e9db7b3e5a2dc8856b14b8
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-12 20:15:52 +0100

    👨🏻‍💻 Worked on some modules

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -3,12 +3,12 @@ use rand::Rng;
 fn main() {
     println!("Guess the number!");
     println!("Please input your guess.");
-
+    
     let secret_number = rand::thread_rng().gen_range(1..101);
-    println!("Your secret number is {}", secret_number);
-
+    
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Unable to read line from stdin");
-
+    
+    println!("Your secret number is {}", secret_number);
     println!("Your guessed value is: {}", guess);
 }
