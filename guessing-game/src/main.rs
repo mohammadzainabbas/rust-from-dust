3420785669b349e5ee591a96c5dacd05853a777b
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-13 09:59:47 +0100

    📲 Enhanced new code

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -10,36 +10,5 @@ fn main() {
 
     println!("Let's play some guessing games");
 
-    let _name: String = Input::with_theme(&ColorfulTheme::default()).with_prompt("Enter your name: ").interact_text().unwrap();
-    let secret_number: u32 = rand::thread_rng().gen_range(1..101);
-    println!("Your secret number is {}", secret_number);
 
-    loop {
-        let guess: u32 = Input::with_theme(&ColorfulTheme::default())
-            .with_prompt("Guess a number: ")
-            .validate_with(|input: &String| -> Result<(), &str> {
-                if input.trim().parse::<u32>().is_ok() {
-                    Ok(())
-                } else {
-                    Err("Please enter a valid number")
-                }
-            })
-            .interact_text()
-            .unwrap()
-            .trim()
-            .parse()
-            .expect("Failed to parse to u32");
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Less"),
-            Ordering::Greater => println!("Greater"),
-            Ordering::Equal => println!("Eq"),
-        }
-    }
-    
-    // let mut guess = String::new();
-    // io::stdin().read_line(&mut guess).expect("Unable to read line from stdin");
-    
-
-    // println!("Your guessed value is: {}", guess);
 }
