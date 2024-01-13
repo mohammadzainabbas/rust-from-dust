0050e5c2589bdf027645b04b3e03661ebd80cbbe
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-13 13:16:55 +0100

    🔥 Removed unused module

--- guessing-game/src/guessing_games.rs
+++ guessing-game/src/guessing_games.rs
@@ -24,9 +24,14 @@ pub fn guess_the_number(theme: &ColorfulTheme) {
             }
         }
     }
+
+    let secret_number: u32 = rand::thread_rng().gen_range(min..=max);
+    
     
     let guess: u32 = validate::valid_int(theme, format!("Guess a number b/w {} & {}: ", min.to_string().cyan().bold(), max.to_string().cyan().bold()));
     // let guess: u32 = validate::valid_int("Guess a number");
+
+
 
 
     // let _name: String = Input::with_theme(&ColorfulTheme::default()).with_prompt("Enter your name: ").interact_text().unwrap();
