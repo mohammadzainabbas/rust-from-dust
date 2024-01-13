5a02ce7826648505b4dc7427ce07d0e8bb6d456d
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-13 12:31:06 +0100

    🔧 Fixed minor bugs in the code

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -25,7 +25,7 @@ fn play_games(theme: &ColorfulTheme) {
             .interact()
             .unwrap();
     
-        term.set_title("Game Menu");
+        term.set_title(TERMINAL_TITLE);
         let (game_name, game_fn) = games[selection];
         
         utils::colored_println(format!("{}{}", "You have picked: ".bright_cyan(), game_name.green().bold()).italic());
