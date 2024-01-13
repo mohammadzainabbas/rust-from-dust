8667c50f5e37ff76c2b6de593419615de0b5aaa7
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-13 15:26:46 +0100

    ✨ New feature added to enhance user experience

--- guessing-game/src/guessing_games.rs
+++ guessing-game/src/guessing_games.rs
@@ -63,5 +63,5 @@ pub fn guess_the_number(theme: &ColorfulTheme, term: &Term) {
 
 #[allow(unused)]
 pub fn guess_the_word(theme: &ColorfulTheme, term: &Term) {
-    println!("Let's play 'Guess the word!' game");
+    utils::colored_println(format!("🚀 Let's play '{}' game.", "Guess the word!".cyan().bold()).bright_yellow().italic());
 }
