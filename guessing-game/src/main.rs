3780be7a70c1b8d3f3d8dec9a2f69983d60e1587
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-13 10:15:13 +0100

    💾 Saved changes before switching branches

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -5,7 +5,7 @@ mod guessing_games;
 
 fn main() {
 
-    println!("{}", "Let's play some guessing games".blue().on_black());
+    println!("{}", "Let's play some guessing games".bright_blue().on_black());
 
     println!("Hello");
 
