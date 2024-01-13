34e3d21dc0ef9fb923b15f650468d42281ad65c1
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-13 16:42:56 +0100

    📚 Updated an improvement

--- guessing-game/src/guessing_games.rs
+++ guessing-game/src/guessing_games.rs
@@ -92,6 +92,7 @@ pub fn guess_the_word(theme: &ColorfulTheme, term: &Term) {
 
     if words.is_empty() {
         utils::colored_print("No word entered! Leaving the game...".bright_red().italic())
+        return;
     }
 
     words.sort_by(|a, b| a.to_lowercase().cmp(&b.to_lowercase())); // case-insensitive sorting
