1faa0fbbc07def36eccb47835e0f85fe3890e76a
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-13 09:03:36 +0100

    🐛 Fixed minor typos in some code

--- guessing-game/src/validator.rs
+++ guessing-game/src/validator.rs
@@ -1,3 +1,16 @@
 pub fn valid_u32() {
-
+    let guess: u32 = Input::with_theme(&ColorfulTheme::default())
+            .with_prompt("Guess a number: ")
+            .validate_with(|input: &String| -> Result<(), &str> {
+                if input.trim().parse::<u32>().is_ok() {
+                    Ok(())
+                } else {
+                    Err("Please enter a valid number")
+                }
+            })
+            .interact_text()
+            .unwrap()
+            .trim()
+            .parse()
+            .expect("Failed to parse to u32");
 }
