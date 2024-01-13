9c1d40e82291cb2f07036828e5dbbc83e4ec1112
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-13 12:14:41 +0100

    🌲 Cleaned up improvement

--- guessing-game/src/utils.rs
+++ guessing-game/src/utils.rs
@@ -3,3 +3,7 @@ use colored::ColoredString;
 pub fn colored_print(text: ColoredString) {
     println!("\n{}\n", text);
 }
+
+pub fn colored_print(text: ColoredString) {
+    println!("\n{}\n", text);
+}
