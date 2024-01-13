48bde35f7e773b0e40e602d39e624e0952acd32a
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-13 12:47:10 +0100

    🚀 Deployed new features

--- guessing-game/src/guessing_games.rs
+++ guessing-game/src/guessing_games.rs
@@ -8,7 +8,8 @@ use crate::validate;
 #[allow(unused)]
 pub fn guess_the_number(theme: &ColorfulTheme) {
 
-    let min: u32 = validate::valid_int(format!("Pick {} threshold: ", "min".cyan().bold()).italic());
+    let min: u32 = validate::valid_int(format!("Pick {} threshold: ", "min".cyan().bold()));
+    let max: u32 = validate::valid_int(format!("Pick {} threshold: ", "max".cyan().bold()));
     let _guess: u32 = validate::valid_int("Guess a number");
 
     println!("Let's play 'Guess the number!' game");
