23c9249d776fe77c1004222314f8ae85fdc2b087
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-13 15:50:43 +0100

    🧹 Removed deprecated code and files

--- guessing-game/src/guessing_games.rs
+++ guessing-game/src/guessing_games.rs
@@ -73,7 +73,8 @@ pub fn guess_the_word(theme: &ColorfulTheme, term: &Term) {
     loop {
         // Prompt the user for a word
         let word: String = Input::with_theme(theme)
-            .with_prompt("Enter a word (type :q to finish):")        
+            // .with_prompt("Enter a word (type :q to finish):")        
+            .with_prompt(format!("Pick {} threshold: ", "min".cyan().bold()))        
             .interact()
             .unwrap();
 
