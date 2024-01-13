348b651c4571d51d52f354d76af787b1f99e9825
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-13 12:57:35 +0100

    🌟 Added some updates

--- guessing-game/src/guessing_games.rs
+++ guessing-game/src/guessing_games.rs
@@ -25,35 +25,35 @@ pub fn guess_the_number(theme: &ColorfulTheme) {
 
     println!("Let's continue 'Guess the number!' game");
 
-    let _guess: u32 = validate::valid_int("Guess a number");
+    // let _guess: u32 = validate::valid_int("Guess a number");
 
 
-    let _name: String = Input::with_theme(&ColorfulTheme::default()).with_prompt("Enter your name: ").interact_text().unwrap();
-    let secret_number: u32 = rand::thread_rng().gen_range(1..101);
-    println!("Your secret number is {}", secret_number);
+    // let _name: String = Input::with_theme(&ColorfulTheme::default()).with_prompt("Enter your name: ").interact_text().unwrap();
+    // let secret_number: u32 = rand::thread_rng().gen_range(1..101);
+    // println!("Your secret number is {}", secret_number);
 
-    loop {
-        let guess: u32 = Input::with_theme(&ColorfulTheme::default())
-            .with_prompt("Guess a number: ")
-            .validate_with(|input: &String| -> Result<(), &str> {
-                if input.trim().parse::<u32>().is_ok() {
-                    Ok(())
-                } else {
-                    Err("Please enter a valid number")
-                }
-            })
-            .interact_text()
-            .unwrap()
-            .trim()
-            .parse()
-            .expect("Failed to parse to u32");
+    // loop {
+    //     let guess: u32 = Input::with_theme(&ColorfulTheme::default())
+    //         .with_prompt("Guess a number: ")
+    //         .validate_with(|input: &String| -> Result<(), &str> {
+    //             if input.trim().parse::<u32>().is_ok() {
+    //                 Ok(())
+    //             } else {
+    //                 Err("Please enter a valid number")
+    //             }
+    //         })
+    //         .interact_text()
+    //         .unwrap()
+    //         .trim()
+    //         .parse()
+    //         .expect("Failed to parse to u32");
 
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Less"),
-            Ordering::Greater => println!("Greater"),
-            Ordering::Equal => println!("Eq"),
-        }
-    }
+    //     match guess.cmp(&secret_number) {
+    //         Ordering::Less => println!("Less"),
+    //         Ordering::Greater => println!("Greater"),
+    //         Ordering::Equal => println!("Eq"),
+    //     }
+    // }
     
     // let mut guess = String::new();
     // io::stdin().read_line(&mut guess).expect("Unable to read line from stdin");
