5b081ab86c8600e69d16bbeb5ffcb16cb79514b4
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-13 08:40:31 +0100

    🌍 Made some code

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let secret_number: u32 = rand::thread_rng().gen_range(1..101);
 
     loop {
-        let guess: String = Input::with_theme(&ColorfulTheme::default())
+        let guess: u32 = Input::with_theme(&ColorfulTheme::default())
             .with_prompt("Guess a number: ")
             .validate_with(|input: &String| -> Result<(), &str> {
                 if input.trim().parse::<u32>().is_ok() {
