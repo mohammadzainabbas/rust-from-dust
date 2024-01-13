5ba6e2d595802f5e80c502e8a15da3f7aef03df8
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-13 13:02:59 +0100

    🚀 Deployed code

--- guessing-game/src/guessing_games.rs
+++ guessing-game/src/guessing_games.rs
@@ -26,7 +26,7 @@ pub fn guess_the_number(theme: &ColorfulTheme) {
     println!("Let's continue 'Guess the number!' game");
 
     let guess: u32 = validate::valid_int("Guess a number");
-    let guess: u32 = validate::valid_int(format!("Guess a number b/w {} & {}: ", "min".cyan().bold(), "min".cyan().bold()));
+    let guess: u32 = validate::valid_int(format!("Guess a number b/w {} & {}: ", min.to_string().cyan().bold(), "min".cyan().bold()));
 
 
     // let _name: String = Input::with_theme(&ColorfulTheme::default()).with_prompt("Enter your name: ").interact_text().unwrap();
