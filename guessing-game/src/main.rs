86b56699f51c38658839ed76d9713170abde7636
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-13 10:12:00 +0100

    🔒 Enhanced an improvement

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -5,7 +5,7 @@ mod guessing_games;
 
 fn main() {
 
-    println!("{}", "Let's play some guessing games".green().bold());
+    println!("{}", "Let's play some guessing games".red().bold().on_blue());
 
 
 }
