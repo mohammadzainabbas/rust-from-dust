d9efe2e893651a684290e4498f0cd15dad9aaaa8
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-13 09:15:01 +0100

    🚀 Adding new functionality to meet user demands

--- guessing-game/src/utils.rs
+++ guessing-game/src/utils.rs
@@ -1,18 +1,21 @@
 use dialoguer::{theme::ColorfulTheme, Input};
 
-pub fn valid_int(prompt: String) -> T {
+pub fn valid_int<T>(prompt: &str) -> T
+where
+    T: std::str::FromStr,
+{
     Input::with_theme(&ColorfulTheme::default())
-            .with_prompt(prompt)
-            .validate_with(|input: &String| -> Result<(), &str> {
-                if input.trim().parse::<T>().is_ok() {
-                    Ok(())
-                } else {
-                    Err("Please enter a valid number")
-                }
-            })
-            .interact_text()
-            .unwrap()
-            .trim()
-            .parse()
-            .expect("Failed to parse from string to integer")
+        .with_prompt(prompt)
+        .validate_with(|input: &String| -> Result<(), &str> {
+            if input.trim().parse::<T>().is_ok() {
+                Ok(())
+            } else {
+                Err("Please enter a valid number")
+            }
+        })
+        .interact_text()
+        .unwrap()
+        .trim()
+        .parse()
+        .expect("Failed to parse from string to integer")
 }
