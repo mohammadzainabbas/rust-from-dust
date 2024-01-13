c58e1cbe97882499226e097a69ed061c2646929a
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-13 10:14:51 +0100

    📝 Added comments for complex code segments

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -5,7 +5,7 @@ mod guessing_games;
 
 fn main() {
 
-    println!("{}", "Let's play some guessing games".red().on_black());
+    println!("{}", "Let's play some guessing games".blue().on_black());
 
     println!("Hello");
 
