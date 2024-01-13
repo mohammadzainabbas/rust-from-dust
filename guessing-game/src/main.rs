b01230e9ee45d99dbaadc290b9a772a256656578
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-13 10:44:35 +0100

    🔍 Fixed typos in a new feature

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -23,7 +23,7 @@ fn play_games(theme: &ColorfulTheme) {
 }
 
 fn quit() {
-    println!("\n{}\n", "nevermind then :(".bright_blue().on_black());
+    println!("\n{}\n", "Nevermind then :(".bright_red().on_black());
 }
 
 fn main() {
