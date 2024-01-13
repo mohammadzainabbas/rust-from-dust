26d71955561e3c4fc2ebb3f9611b2240f9ad3bef
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-13 10:40:24 +0100

    📈 Improved performance of existing features

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -42,35 +42,35 @@ fn main() {
 
 
 
-    let term = Term::buffered_stderr();
+    // let term = Term::buffered_stderr();
 
-    Confirm::with_theme(&theme)
-        .with_prompt(format!("{}", "Do you want to continue?".bright_yellow()))
-        .interact_on(&term)
-        .unwrap();
+    // Confirm::with_theme(&theme)
+    //     .with_prompt(format!("{}", "Do you want to continue?".bright_yellow()))
+    //     .interact_on(&term)
+    //     .unwrap();
 
-    let _: String = Input::with_theme(&theme)
-        .with_prompt("Your name")
-        .interact_on(&term)
-        .unwrap();
+    // let _: String = Input::with_theme(&theme)
+    //     .with_prompt("Your name")
+    //     .interact_on(&term)
+    //     .unwrap();
 
-    Select::with_theme(&theme)
-        .with_prompt("Pick an item")
-        .items(items)
-        .interact_on(&term)
-        .unwrap();
+    // Select::with_theme(&theme)
+    //     .with_prompt("Pick an item")
+    //     .items(items)
+    //     .interact_on(&term)
+    //     .unwrap();
 
-    MultiSelect::with_theme(&theme)
-        .with_prompt("Pick some items")
-        .items(items)
-        .interact_on(&term)
-        .unwrap();
+    // MultiSelect::with_theme(&theme)
+    //     .with_prompt("Pick some items")
+    //     .items(items)
+    //     .interact_on(&term)
+    //     .unwrap();
 
-    Sort::with_theme(&theme)
-        .with_prompt("Order these items")
-        .items(items)
-        .interact_on(&term)
-        .unwrap();
+    // Sort::with_theme(&theme)
+    //     .with_prompt("Order these items")
+    //     .items(items)
+    //     .interact_on(&term)
+    //     .unwrap();
 
 
 }
