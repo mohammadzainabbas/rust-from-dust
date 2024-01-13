135377bc03f29e6b8b5e7c706c6156f52514f8de
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-13 11:22:35 +0100

    🧱 Laid groundwork for new features

--- guessing-game/src/utils.rs
+++ guessing-game/src/utils.rs
@@ -1,6 +1,10 @@
 use colored::ColoredString;
 
 pub fn colored_print(text: ColoredString, newline: bool) {
+
+    println!(if newline {} else {})
+
+
     if newline {}
     println!("\n{}\n", text);
 }
