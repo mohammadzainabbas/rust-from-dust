c59b9cfea09e6dbfc5c259b8878f30bc3facc89b
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-13 15:05:56 +0100

    👨🏻‍💻 Worked on some updates

--- guessing-game/src/guessing_games.rs
+++ guessing-game/src/guessing_games.rs
@@ -36,7 +36,7 @@ pub fn guess_the_number(theme: &ColorfulTheme, term: &Term) {
         match guess.cmp(&secret_number) {
             Ordering::Equal => {
                 let tries_str = if guess_tries == 1 { "try" } else { "tries" };
-                utils::colored_println(format!("🎉 Correct guess! You took {} {tries_str} to guess the secret number!", guess_tries.to_string().cyan().bold()).bright_yellow().italic());
+                utils::colored_print(format!("\n🎉 Correct guess! You took {} {tries_str} to guess the secret number!", guess_tries.to_string().cyan().bold()).bright_yellow().italic());
                 break;
             },
             _ => {
