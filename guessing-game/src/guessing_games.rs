da5b15b95b865378ac6fb7e1377d563727269063
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-13 09:37:07 +0100

    📈 Better workflow for new code

--- guessing-game/src/guessing_games.rs
+++ guessing-game/src/guessing_games.rs
@@ -6,7 +6,7 @@ use guessing_game::utils;
 
 pub fn guess_the_number() {
 
-    let guess: u32 = utils::valid_int("Guess a number");
+    let _guess: u32 = utils::valid_int("Guess a number");
 
     println!("Let's play 'Guess the number!' game");
 
