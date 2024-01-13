c6256540d70ec532f7e9c6e6581171b7c97c4a7f
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-13 16:03:25 +0100

    🔧 Tuned performance parameters for a new module

--- guessing-game/src/guessing_games.rs
+++ guessing-game/src/guessing_games.rs
@@ -84,11 +84,11 @@ pub fn guess_the_word(theme: &ColorfulTheme, term: &Term) {
         }
 
         if (words.contains(&word)) {
-            
+            utils::colored_print(format!("You have entered {} already!", word).bright_red().italic());
+        } else {
+            // Add the word to the vector
+            words.push(word);
         }
-
-        // Add the word to the vector
-        words.push(word);
     }
 
     utils::colored_println(format!("Vec: {:?}", words).cyan());
