009efb5896dab4dfa5b9fce3b78d20524b3d3bd9
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-19 16:26:58 +0100

    🚀 Deployed new code

--- rust-axum-intro/src/main.rs
+++ rust-axum-intro/src/main.rs
@@ -6,7 +6,7 @@ use axum::{Router, routing::get, response::Html};
 async fn main() {
     let routes_hello = Router::new().route("/hello", get(
         || async {
-            Html("Hello World")
+            Html("<h3> Hello World </h3>")
         }
     ))
 }
