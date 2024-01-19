056bc80c71c8d9aa968f508851f1c7141049be27
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-19 16:29:46 +0100

    🏎️ Optimizing performance for faster execution

--- rust-axum-intro/src/main.rs
+++ rust-axum-intro/src/main.rs
@@ -1,5 +1,7 @@
 #![allow(unused)] // for dev
 
+use std::net::SocketAddr;
+
 use axum::{response::Html, routing::get, Router};
 
 #[tokio::main(worker_threads = 2)]
@@ -7,5 +9,5 @@ async fn main() {
     let routes_hello =
         Router::new().route("/hello", get(|| async { Html("<h3> Hello World </h3>") }));
 
-    let socket = Sock
+    let socket = SocketAddr::new(ip, port)
 }
