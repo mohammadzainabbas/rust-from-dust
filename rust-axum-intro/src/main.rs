5076136ce590ce524c9130f872985ece34670db1
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-19 16:26:14 +0100

    🎉 Celebrated completing some features

--- rust-axum-intro/src/main.rs
+++ rust-axum-intro/src/main.rs
@@ -1,12 +1,12 @@
 // #![allow(unused)] // for dev
 
-use axum::{Router, routing::get};
+use axum::{Router, routing::get, response::Html};
 
 #[tokio::main(worker_threads = 2)]
 async fn main() {
     let routes_hello = Router::new().route("/hello", get(
         || async {
-            
+            Html()
         }
     ))
 }
