139d22055b67199c453a725feca4c5eaec064fcf
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-19 18:26:08 +0100

    🎭 Added support for some requirements

--- rust-axum-intro/src/main.rs
+++ rust-axum-intro/src/main.rs
@@ -6,13 +6,9 @@ use axum::{response::Html, routing::get, Router};
 
 #[tokio::main(worker_threads = 2)]
 async fn main() {
-    let routes_hello =
-        Router::new().route("/hello", get(|| async { Html("<h3> Hello World </h3>") }));
-
-    let address = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
     println!("Listening on {}", address);
 
-    let router = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let router = Router::new().route("/hello", get(|| async { Html("<h3> Hello World </h3>") }));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, router).await.unwrap();
