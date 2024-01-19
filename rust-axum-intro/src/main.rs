e97219de248e6db797d2eef8802b4bb75f732346
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-19 18:26:49 +0100

    🔒 Enhanced security measures in the application

--- rust-axum-intro/src/main.rs
+++ rust-axum-intro/src/main.rs
@@ -6,12 +6,9 @@ use axum::{response::Html, routing::get, Router};
 
 #[tokio::main(worker_threads = 2)]
 async fn main() {
-    println!("Listening on {}", address);
-
     let router = Router::new().route("/hello", get(|| async { Html("<h3> Hello World </h3>") }));
-
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Listening on {:#?}", listener);
     axum::serve(listener, router).await.unwrap();
-
     // axum::Serve::bind(&address).serve(routes_hello)
 }
