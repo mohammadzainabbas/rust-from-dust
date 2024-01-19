9a39ebae7f6bfeb6cb9746c51f98a53c666379a2
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-19 16:25:52 +0100

    🌍 Made code

--- rust-axum-intro/src/main.rs
+++ rust-axum-intro/src/main.rs
@@ -5,6 +5,8 @@ use axum::{Router, routing::get};
 #[tokio::main(worker_threads = 2)]
 async fn main() {
     let routes_hello = Router::new().route("/hello", get(
-        || async
+        || async {
+            
+        }
     ))
 }
