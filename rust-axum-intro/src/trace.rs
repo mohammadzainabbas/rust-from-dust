d7f2a49d2cb6dc80a95af023acd80da328d7e2b8
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-05 00:53:34 +0100

    🚧 Work in progress on new code

--- rust-axum-intro/src/trace.rs
+++ rust-axum-intro/src/trace.rs
@@ -1,7 +1,12 @@
 use tracing::{trace, Level};
 use tracing_appender::rolling;
 use tracing_subscriber::fmt::writer::MakeWriterExt;
-
+/// Fn [`setup_tracing`] creates three basic routes, returns a [`axum::Router`](https://docs.rs/axum/latest/axum/struct.Router.html) object.
+///
+/// - `/` with [`axum::routing::get`](https://docs.rs/axum/latest/axum/routing/method_routing/fn.get.html) method
+/// - `/hello` with [`axum::routing::get`](https://docs.rs/axum/latest/axum/routing/method_routing/fn.get.html) method
+/// - `/hello/:path` with [`axum::routing::get`](https://docs.rs/axum/latest/axum/routing/method_routing/fn.get.html) method
+///
 pub async fn setup_tracing() {
     let log_dir = "./logs";
     let debug_file = rolling::daily(log_dir, "debug").with_min_level(Level::WARN);
