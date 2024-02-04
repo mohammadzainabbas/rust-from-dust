661aadea3345a310b5558f50e2009e48f015f7d6
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-04 11:32:20 +0100

    📲 Enhanced an update

---
+++ rust-axum-intro/tests/hello.rs
@@ -0,0 +1,34 @@
+#[allow(unused)]
+use anyhow::{Error, Result};
+use serde_json::json;
+
+#[tokio::test]
+async fn test_hello() -> Result<()> {
+    let hc = httpc_test::new_client("http://localhost:3000")?;
+
+    // ZOMBIES (checklist when writing tests)
+    // Link: https://youtu.be/0_UttFDnV3k?t=3539
+    //
+    // Anything that takes a sequence (vector, array etc), test 'em with ZOM
+    //
+    // Z - zero
+    // O - one
+    // M - many
+    // B - boundary conditions
+    // I - interface
+    // E - exceptions
+    // S - simple scenarios
+
+    // 1. Create TODOs
+    hc.do_post("/todo", json!({"text": "Complete this project"}))
+        .await?
+        .print()
+        .await?;
+
+    // 2. Read TODOs
+    hc.do_get("/todo").await?.print().await?;
+
+    // 3. Update TODOs
+
+    Ok(())
+}
