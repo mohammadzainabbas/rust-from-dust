e1945babee811f8929b28b3468fc68c9141ef53a
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-04 08:00:55 +0100

    📦 Packaged new requirement

--- rust-axum-intro/src/main.rs
+++ rust-axum-intro/src/main.rs
@@ -65,8 +65,8 @@ struct HelloParams {
 
 impl Value for HelloParams {
     fn record(&self, key: &tracing::field::Field, visitor: &mut dyn tracing::field::Visit) {
-        let name = self.name;
+        let name = self.name.as_deref();
 
-        visitor.record_str(field, self.name.to_owned())
+        visitor.record_str(field, .to_owned())
     }
 }
