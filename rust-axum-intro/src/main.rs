30d56fc2b4bde0b561681e960d9f5ba4ff9ed8bb
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-04 08:42:30 +0100

    🧹 Cleaned up unnecessary code

--- rust-axum-intro/src/main.rs
+++ rust-axum-intro/src/main.rs
@@ -36,8 +36,7 @@ async fn main() {
 
     let router = Router::new()
         .route("/", get(groot))
-        .route("/hello", get(say_hello))
-        .route("/hello/:path", get(say_path));
+        .route("/hello", get(say_hello));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     info!("server listening on {:#?}", listener.local_addr().unwrap());
     axum::serve(listener, router).await.unwrap();
