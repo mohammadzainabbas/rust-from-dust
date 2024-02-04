c72fec548c15df66f71fe28de1fcf70bf31396f0
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-05 00:43:14 +0100

    🔍 Fixed typos in new features

--- rust-axum-intro/src/hello.rs
+++ rust-axum-intro/src/hello.rs
@@ -34,9 +34,9 @@ async fn say_path(Path(path): Path<String>) -> impl IntoResponse {
 
 /// Fn [`Basic Router`](basic_router) creates three basic routes, returns a [`axum::Router`](https://docs.rs/axum/latest/axum/struct.Router.html) object.
 ///
-/// - `/` : calls [`groot`] fn
-/// - `/hello` : calls [`say_hello`] fn
-/// - `/hello/:path` : calls [`say_path`] fn
+/// - `/` calls [`groot`] fn
+/// - `/hello` calls [`say_hello`] fn
+/// - `/hello/:path` calls [`say_path`] fn
 ///
 pub async fn basic_router() -> Router {
     Router::new()
