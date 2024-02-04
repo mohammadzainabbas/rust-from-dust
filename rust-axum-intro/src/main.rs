ac97613fb47687623ac822d88d4c28a95fb00af8
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-04 08:27:22 +0100

    ⛔️ Removed unnecessary requirement

--- rust-axum-intro/src/main.rs
+++ rust-axum-intro/src/main.rs
@@ -50,7 +50,7 @@ async fn groot() -> Html<&'static str> {
 
 #[tracing::instrument]
 async fn say_hello(Query(param): Query<HelloParams>) -> Response {
-    trace!("say_hello()");
+    trace!("inside say_hello()");
     let name = param.name.as_deref().unwrap_or("World");
     debug!(target: "say", name);
     Html(format!("<h3> Hello {}! </h3>", name)).into_response()
@@ -58,7 +58,7 @@ async fn say_hello(Query(param): Query<HelloParams>) -> Response {
 
 #[tracing::instrument]
 async fn say_path(Path(path): Path<String>) -> impl IntoResponse {
-    trace!("say_path()");
+    trace!("inside say_path()");
     Html(format!("<h3> Hello {}! </h3>", path.as_str())).into_response()
 }
 
