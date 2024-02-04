553692910fad4767d00044012a1e1ce5819f26dc
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-04 23:58:01 +0100

    👨🏻‍💻 Worked on an improved code

--- rust-axum-intro/src/router.rs
+++ rust-axum-intro/src/router.rs
@@ -1,4 +1,8 @@
+use crate::{basic_router, setup_tracing, todo_router};
 use axum::Router;
-use rust_axum_intro::{basic_router, setup_tracing, todo_router};
 
-pub async fn routers() -> Router {}
+pub async fn routers() -> Router {
+    let basic_router = basic_router().await;
+    let todo_routers = todo_router().await;
+    Router::new().merge(basic_router).merge(todo_routers)
+}
