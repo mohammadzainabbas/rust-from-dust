64294bb03bc8f9ce88496ad1e826229fb6610744
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-04 05:47:24 +0100

    🚧 Work in progress on a feature

--- rust-axum-intro/src/main.rs
+++ rust-axum-intro/src/main.rs
@@ -10,16 +10,16 @@ use axum::{
 
 #[tokio::main(worker_threads = 2)]
 async fn main() {
-    let router = Router::new().route("/hello", get(hello_world));
+    let router = Router::new().route("/", get(groot).route("/hello", get(hello_world));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("Listening on {:#?}", listener.local_addr().unwrap());
     axum::serve(listener, router).await.unwrap();
 }
 
-async fn hello_world() -> Response {
-    Html("<h3> Hello World! </h3>").into_response()
-}
-
 async fn groot() -> &'static str {
     "Hello, I'm groot!"
+}
+
+async fn hello_world() -> Response {
+    Html("<h3> Hello World! </h3>").into_response()
 }
