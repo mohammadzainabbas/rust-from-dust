cf7c310c566b041d47e3ecc9f430b1f2d1ae65aa
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-04 07:52:44 +0100

    🛡️ Strengthened an improvement

--- rust-axum-intro/src/main.rs
+++ rust-axum-intro/src/main.rs
@@ -48,6 +48,8 @@ async fn groot() -> Html<&'static str> {
 }
 
 async fn say_hello(Query(param): Query<HelloParams>) -> Response {
+    trace!("inside groot()");
+    debug!("{param}");
     let name = param.name.as_deref().unwrap_or("World");
     Html(format!("<h3> Hello {}! </h3>", name)).into_response()
 }
