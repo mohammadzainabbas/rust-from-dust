4015302a7cc300965529d064092b57075bc7d613
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-05 00:10:57 +0100

    🚀 Deployed an improved code

--- rust-axum-intro/src/router.rs
+++ rust-axum-intro/src/router.rs
@@ -3,8 +3,10 @@ use axum::Router;
 
 /// [get_routers] creates app's routers
 ///
-/// A function that provides all app's routers makes it easy to call it from tests
-/// without having to create an HTTP server.
+/// [get_routers] merges two routers: [basic_router] and [todo_router] into one and returns.
+///
+/// > A function that provides all app's routers makes it easy to call it from tests
+/// > without having to create an HTTP server.
 pub async fn get_routers() -> Router {
     let basic_router = basic_router().await;
     let todo_routers = todo_router().await;
