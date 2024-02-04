441ba9a9701d7b4bb0704332e3233cdae50124ec
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-04 23:54:22 +0100

    🐛 Fixed minor typos in a new module

--- rust-axum-intro/src/lib.rs
+++ rust-axum-intro/src/lib.rs
@@ -3,3 +3,6 @@ pub use todos::todo_router;
 
 mod hello;
 pub use hello::basic_router;
+
+mod trace;
+pub use trace::
--- rust-axum-intro/src/trace.rs
+++ rust-axum-intro/src/trace.rs
@@ -2,7 +2,7 @@ use tracing::{info, trace, Level};
 use tracing_appender::rolling;
 use tracing_subscriber::fmt::writer::MakeWriterExt;
 
-async fn setup_tracing() {
+pub async fn setup_tracing() {
     let log_dir = "./logs";
     let debug_file = rolling::daily(log_dir, "debug").with_min_level(Level::WARN);
     let warning_file = rolling::daily(log_dir, "warning").with_max_level(Level::WARN);
