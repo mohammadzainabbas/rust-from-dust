f757ffb1efc9ef8d4fb3998da64933654e1fda05
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-04 11:34:07 +0100

    📈 Better workflow for a module

---
+++ rust-axum-intro/tests/main_dev.rs
@@ -0,0 +1,11 @@
+#[allow(unused)]
+use anyhow::{Error, Result};
+
+#[tokio::test]
+async fn quick_dev() -> Result<()> {
+    let hc = httpc_test::new_client("http://localhost:3000")?;
+
+    hc.do_get("/").await?.print().await?;
+
+    Ok(())
+}
