be5d4bbc67b2ac096dcca11e16346cfffea1783c
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-05 00:08:09 +0100

    🔍 Fixed typos in some code

--- rust-axum-intro/src/router.rs
+++ rust-axum-intro/src/router.rs
@@ -1,6 +1,7 @@
 use crate::{basic_router, todo_router};
 use axum::Router;
 
+/// *get_routers()* calls  
 pub async fn get_routers() -> Router {
     let basic_router = basic_router().await;
     let todo_routers = todo_router().await;
