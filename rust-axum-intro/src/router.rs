0fef6fd399ce49777f3b3d803c594ef648bc5314
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-05 00:17:43 +0100

    📈 Improved performance of existing features

--- rust-axum-intro/src/router.rs
+++ rust-axum-intro/src/router.rs
@@ -1,12 +1,10 @@
 use crate::{basic_router, todo_router};
 use axum::Router;
 
-/// [get_routers] creates app's routers
+/// [`get_routers`] merges two routers: [`basic_router`] and [`todo_router`] into one and returns.
 ///
-/// [get_routers] merges two routers: [basic_router] and [todo_router] into one and returns.
-///
-/// > A function that provides all app's routers makes it easy to call it from tests
-/// > without having to create an HTTP server.
+/// Note: A function that provides all app's routers makes it easy to call it from tests
+/// without having to create an HTTP server.
 ///
 pub async fn get_routers() -> Router {
     let basic_router = basic_router().await;
