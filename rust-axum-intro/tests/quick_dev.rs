d53c94381b31f1a00e62799e6c27c5ab1bb1e122
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-04 10:36:13 +0100

    🐛 Fixed minor typos in new features

--- rust-axum-intro/tests/quick_dev.rs
+++ rust-axum-intro/tests/quick_dev.rs
@@ -6,9 +6,10 @@ async fn quick_dev() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:3000")?;
 
     hc.do_get("/").await?.print().await?;
-    hc.do_get("/hello").await?.print().await?;
-    hc.do_get("/hello/what").await?.print().await?;
-    hc.do_get("/hello?name=Mohammad").await?.print().await?;
+    // hc.do_get("/hello").await?.print().await?;
+    // hc.do_get("/hello/what").await?.print().await?;
+    // hc.do_get("/hello?name=Mohammad").await?.print().await?;
 
+    hc.do_get("/todo").await?.print().await?;
     Ok(())
 }
