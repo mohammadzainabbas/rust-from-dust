8e348a48f508235c0ad52d46b8db48b349786f1e
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-04 06:02:45 +0100

    👨🏻‍💻 Worked on an update

--- rust-axum-intro/tests/quick_dev.rs
+++ rust-axum-intro/tests/quick_dev.rs
@@ -7,7 +7,7 @@ async fn quick_dev() -> Result<()> {
 
     hc.do_get("/").await?.print().await?;
     hc.do_get("/hello").await?.print().await?;
-    hc.do_get("/hello?name=Zain").await?.print().await?;
+    hc.do_get("/hello?name=Mohammad").await?.print().await?;
 
     Ok(())
 }
