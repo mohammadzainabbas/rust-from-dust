7460d0a0f5a0126cb6a88a352f7b799ac3026439
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-04 08:37:39 +0100

    🕵🏻 Conducted code review for some improvements

--- rust-axum-intro/src/todos.rs
+++ rust-axum-intro/src/todos.rs
@@ -37,3 +37,10 @@ struct UpdateTodo {
     text: Option<String>,
     completed: Option<bool>,
 }
+
+// The query parameters for todos index
+#[derive(Debug, Deserialize, Default)]
+pub struct Pagination {
+    pub offset: Option<usize>,
+    pub limit: Option<usize>,
+}
