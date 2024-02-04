63d636e3a112244f93453ebebab4e200cc413c88
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-04 10:42:47 +0100

    🌍 Made an improved code

--- rust-axum-intro/tests/todos.rs
+++ rust-axum-intro/tests/todos.rs
@@ -3,13 +3,11 @@ use anyhow::{Error, Result};
 use serde_json::json;
 
 #[tokio::test]
-async fn quick_dev() -> Result<()> {
+async fn test_todos() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:3000")?;
 
-    hc.do_get("/").await?.print().await?;
-    // hc.do_get("/hello").await?.print().await?;
-    // hc.do_get("/hello/what").await?.print().await?;
-    // hc.do_get("/hello?name=Mohammad").await?.print().await?;
+    // ZOMBIE
+    //
 
     hc.do_get("/todo").await?.print().await?;
     hc.do_post("/todo", json!({"text": "Complete this project"}))
