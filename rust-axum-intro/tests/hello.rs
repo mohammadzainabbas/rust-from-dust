181139bf06af3a57a5aff290ca465815a66c0351
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-04 11:36:14 +0100

    🔒 Enhanced new code

--- rust-axum-intro/tests/hello.rs
+++ rust-axum-intro/tests/hello.rs
@@ -1,22 +1,22 @@
 #[allow(unused)]
 use anyhow::{Error, Result};
 
+// ZOMBIES (checklist when writing tests)
+// Link: https://youtu.be/0_UttFDnV3k?t=3539
+//
+// Anything that takes a sequence (vector, array etc), test 'em with ZOM
+//
+// Z - zero
+// O - one
+// M - many
+// B - boundary conditions
+// I - interface
+// E - exceptions
+// S - simple scenarios
+
 #[tokio::test]
 async fn test_hello() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:3000")?;
-
-    // ZOMBIES (checklist when writing tests)
-    // Link: https://youtu.be/0_UttFDnV3k?t=3539
-    //
-    // Anything that takes a sequence (vector, array etc), test 'em with ZOM
-    //
-    // Z - zero
-    // O - one
-    // M - many
-    // B - boundary conditions
-    // I - interface
-    // E - exceptions
-    // S - simple scenarios
 
     hc.do_get("/").await?.print().await?;
     hc.do_get("/hello").await?.print().await?;
