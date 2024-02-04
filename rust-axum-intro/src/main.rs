0b9b0393a856cab07943efbd44cece5636788227
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-04 10:15:27 +0100

    🚀 Deployed some features

--- rust-axum-intro/src/main.rs
+++ rust-axum-intro/src/main.rs
@@ -44,6 +44,11 @@ async fn main() {
 
     let db = DB::default();
 
+    Router::new()
+    .route("/", get(groot))
+    .route("/hello", get(say_hello))
+    .route("/hello/:path", get(say_path))
+
     let todo_routers = Router::new()
         .route("/todo", get(read_todos).post(create_todo))
         .route("/todo/:id", patch(update_todo).delete(delete_todo));
