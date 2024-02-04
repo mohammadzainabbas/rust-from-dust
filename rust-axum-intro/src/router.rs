fb6c477ad57ecd6b9d4632a9655a88c216719318
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-05 00:08:51 +0100

    🔧 Tuned performance parameters for some updates

--- rust-axum-intro/src/router.rs
+++ rust-axum-intro/src/router.rs
@@ -1,7 +1,9 @@
 use crate::{basic_router, todo_router};
 use axum::Router;
 
-/// *get_routers()* calls  
+/// *get_routers()* calls
+/// A function that provides all app's routers makes it easy to call it from tests
+/// without having to create an HTTP server.
 pub async fn get_routers() -> Router {
     let basic_router = basic_router().await;
     let todo_routers = todo_router().await;
