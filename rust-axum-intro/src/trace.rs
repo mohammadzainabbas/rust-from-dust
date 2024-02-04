f2cb4776c87249de77fc633fa3e2bd3a341385a1
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-04 23:53:38 +0100

    🧱 Laid groundwork for some modules

---
+++ rust-axum-intro/src/trace.rs
@@ -0,0 +1,19 @@
+use tracing::{info, trace, Level};
+use tracing_appender::rolling;
+use tracing_subscriber::fmt::writer::MakeWriterExt;
+
+async fn setup_tracing() {
+    let log_dir = "./logs";
+    let debug_file = rolling::daily(log_dir, "debug").with_min_level(Level::WARN);
+    let warning_file = rolling::daily(log_dir, "warning").with_max_level(Level::WARN);
+    let log_files = debug_file.and(warning_file);
+
+    tracing_subscriber::fmt()
+        .json()
+        .with_writer(log_files)
+        .with_ansi(true)
+        .with_max_level(Level::TRACE)
+        .init();
+
+    trace!("setup_tracing() done!");
+}
