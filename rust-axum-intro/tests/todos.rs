0485ed7cf49ebeb8865794a2ef75425623b74370
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-04 11:36:35 +0100

    💾 Saved changes for code

--- rust-axum-intro/tests/todos.rs
+++ rust-axum-intro/tests/todos.rs
@@ -2,22 +2,22 @@
 use anyhow::{Error, Result};
 use serde_json::json;
 
+// ZOMBIES (checklist when writing tests)
+// Link: https://youtu.be/0_UttFDnV3k?t=3539
+//
+// Anything that takes a sequence (vector, array etc), test 'em with ZOM
+//
+// Z - zero
+// O - one
+// M - many
+// B - boundary conditions
+// I - interface
+// E - exceptions
+// S - simple scenarios
+
 #[tokio::test]
 async fn test_todos() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:3000")?;
-
-    // ZOMBIES (checklist when writing tests)
-    // Link: https://youtu.be/0_UttFDnV3k?t=3539
-    //
-    // Anything that takes a sequence (vector, array etc), test 'em with ZOM
-    //
-    // Z - zero
-    // O - one
-    // M - many
-    // B - boundary conditions
-    // I - interface
-    // E - exceptions
-    // S - simple scenarios
 
     // 1. Create TODOs
     hc.do_post("/todo", json!({"text": "Complete this project"}))
