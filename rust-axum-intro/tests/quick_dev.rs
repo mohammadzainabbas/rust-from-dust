11870b680e38c832c785f7fd5b30cac767209247
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-04 04:29:44 +0100

    🚧 Work in progress on code

--- rust-axum-intro/tests/quick_dev.rs
+++ rust-axum-intro/tests/quick_dev.rs
@@ -3,6 +3,6 @@ use anyhow::{Error, Result};
 
 #[tokio::test]
 async fn quick_dev() -> Result<()> {
-    let hc = httpc_test::new_client("http://localhost:3000");
+    let hc = httpc_test::new_client("http://localhost:3000")?;
     hc.Ok(())
 }
