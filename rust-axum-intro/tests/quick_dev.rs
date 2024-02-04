b4a6240a01a88138231511435b0441fcbdcc4334
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-04 10:39:17 +0100

    📲 Enhanced some code

--- rust-axum-intro/tests/quick_dev.rs
+++ rust-axum-intro/tests/quick_dev.rs
@@ -11,6 +11,6 @@ async fn quick_dev() -> Result<()> {
     // hc.do_get("/hello?name=Mohammad").await?.print().await?;
 
     hc.do_get("/todo").await?.print().await?;
-    hc.do_post("/todo", Jso)
+    hc.do_post("/todo", Json({text: "Complete this project"})).await?.print().await?;
     Ok(())
 }
