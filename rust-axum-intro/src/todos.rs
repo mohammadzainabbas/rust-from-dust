28cf0f9b9bf4e3995765feacc80ad42e4388551f
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-04 08:36:14 +0100

    🔒 Enhanced some code

--- rust-axum-intro/src/todos.rs
+++ rust-axum-intro/src/todos.rs
@@ -1,7 +1,21 @@
+#[allow(unused)]
+use axum::{
+    error_handling::HandleErrorLayer,
+    extract::{Path, Query, State},
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, patch},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
 use std::{
     collections::HashMap,
     sync::{Arc, RwLock},
     time::Duration,
 };
+use tower::{BoxError, ServiceBuilder};
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use uuid::Uuid;
 
 type Db = Arc<RwLock<HashMap<Uuid, Todo>>>;
