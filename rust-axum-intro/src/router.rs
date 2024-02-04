258a40c6c9b49ec0d168ce8584c0221331084132
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-05 00:30:31 +0100

    ✅ Passed all tests for some improvements

--- rust-axum-intro/src/router.rs
+++ rust-axum-intro/src/router.rs
@@ -1,7 +1,7 @@
 use crate::{basic_router, todo_router};
 use axum::Router;
 
-/// [`get_routers`] merges two routers: [`basic_router`] and [`todo_router`] into one and returns.
+/// Fn [`get_routers`] merges two routers: [`Basic Router`](basic_router) and [`Todo Router`](todo_router) into one and returns.
 ///
 /// Note: A function that provides all app's routers makes it easy to call it from tests
 /// without having to create an HTTP server.
