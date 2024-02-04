d4ac173c3e3fd9a2df178f54adcdaa6046dca9d6
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-05 00:09:35 +0100

    🎉 Celebrated completing a module

--- rust-axum-intro/src/router.rs
+++ rust-axum-intro/src/router.rs
@@ -1,7 +1,7 @@
 use crate::{basic_router, todo_router};
 use axum::Router;
 
-/// ['get_routers'] creates app's routers
+/// [get_routers] creates app's routers
 ///
 /// A function that provides all app's routers makes it easy to call it from tests
 /// without having to create an HTTP server.
