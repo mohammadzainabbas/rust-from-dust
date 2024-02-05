9694d163c473662a11dbb26baabf1b5a88a32e93
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-05 01:57:28 +0100

    🛠️ Refactored an improved code

--- rust-axum-intro/tests/hello.rs
+++ rust-axum-intro/tests/hello.rs
@@ -73,19 +73,18 @@ async fn test_say_hello_default() -> Result<()> {
 
     let uris = vec!["/hello", "/hello?names=Mohammad", "/hello?"];
 
-    for uri in uris {}
-
-    let uri = "/hello?names=Mohammad";
-    let request = Request::builder().uri(uri).body(Body::empty()).unwrap();
-    let response = ServiceExt::<Request<Body>>::ready(&mut routers)
-        .await
-        .unwrap()
-        .call(request)
-        .await
-        .unwrap();
-    assert_eq!(response.status(), StatusCode::OK);
-    let body = response.into_body().collect().await.unwrap().to_bytes();
-    assert_eq!(&body[..], b"<h3> Hello World! </h3>");
+    for uri in uris {
+        let request = Request::builder().uri(uri).body(Body::empty()).unwrap();
+        let response = ServiceExt::<Request<Body>>::ready(&mut routers)
+            .await
+            .unwrap()
+            .call(request)
+            .await
+            .unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        assert_eq!(&body[..], b"<h3> Hello World! </h3>");
+    }
 
     Ok(())
 }
