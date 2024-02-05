f2699ebeebeceacbfd66dcfbdd7fe58a441fde01
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-05 02:35:33 +0100

    🔗 Fixing compatibility issues across platforms

--- rust-axum-intro/tests/todos.rs
+++ rust-axum-intro/tests/todos.rs
@@ -11,6 +11,15 @@
 // E - exceptions
 // S - simple scenarios
 
+use anyhow::Result;
+use axum::{
+    body::Body,
+    http::{Request, StatusCode},
+};
+use http_body_util::BodyExt; // for `collect`
+use rust_axum_intro::get_routers;
+use tower::{Service, ServiceExt}; // for `call`, `oneshot`, and `ready`
+
 #[tokio::test]
 async fn test_todo() -> Result<(), anyhow::Error> {
     Ok(())
