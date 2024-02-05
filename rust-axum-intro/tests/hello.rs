55b88dc2a31ed3d5a668568442be6c1192cfe468
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-05 02:00:12 +0100

    🌲 Cleaned up feature

--- rust-axum-intro/tests/hello.rs
+++ rust-axum-intro/tests/hello.rs
@@ -74,6 +74,20 @@ async fn test_say_hello_default() -> Result<()> {
         assert_eq!(&body[..], b"<h3> Hello World! </h3>");
     }
 
+    futures::future::join_all(uris.into_iter().map(|uri| async move {
+        let request = Request::builder().uri(uri).body(Body::empty()).unwrap();
+        let response = ServiceExt::<Request<Body>>::ready(&mut routers)
+            .await
+            .unwrap()
+            .call(request)
+            .await
+            .unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        assert_eq!(&body[..], b"<h3> Hello World! </h3>");
+    }))
+    .await;
+
     Ok(())
 }
 
