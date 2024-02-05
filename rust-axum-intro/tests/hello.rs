7cd6a0f8629efc8d16528b61ec260f124d55ca71
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-05 02:07:00 +0100

    💾 Saved changes for some requirements

--- rust-axum-intro/tests/hello.rs
+++ rust-axum-intro/tests/hello.rs
@@ -80,7 +80,7 @@ async fn test_say_hello_default() -> Result<()> {
 #[tokio::test]
 async fn test_say_hello() -> Result<()> {
     let mut routers = get_routers().await.into_service();
-    let uris = vec!["/hello", "/hello?names=Mohammad", "/hello?"];
+    let uris = vec![("/hello", "World"), ("/hello?name=Mohammad"), "/hello?"];
 
     for uri in uris {
         let request = Request::builder().uri(uri).body(Body::empty()).unwrap();
