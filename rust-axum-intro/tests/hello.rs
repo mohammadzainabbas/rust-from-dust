2a696169c114be0d1c9cc6fe475ed5640a1b5923
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-05 02:23:48 +0100

    🌍 Made an improved code

--- rust-axum-intro/tests/hello.rs
+++ rust-axum-intro/tests/hello.rs
@@ -30,10 +30,11 @@ use tower::{Service, ServiceExt}; // for `call`, `oneshot`, and `ready`
 async fn test_hello() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:3000")?;
 
-    hc.do_get("/").await?.print().await?;
-    hc.do_get("/hello").await?.print().await?;
-    hc.do_get("/hello/what").await?.print().await?;
-    hc.do_get("/hello?name=Mohammad").await?.print().await?;
+    // hc.do_get("/").await?.print().await?;
+    // hc.do_get("/hello").await?.print().await?;
+    // hc.do_get("/hello/what").await?.print().await?;
+    hc.do_get("/hello/what up").await?.print().await?;
+    // hc.do_get("/hello?name=Mohammad").await?.print().await?;
 
     Ok(())
 }
