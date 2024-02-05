40997ac9b5320e3e730d5a80161093e04000329b
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-05 03:46:21 +0100

    💾 Saved changes before switching branches

--- rust-axum-intro/tests/todos.rs
+++ rust-axum-intro/tests/todos.rs
@@ -48,7 +48,7 @@ async fn test_create_todo() -> Result<(), anyhow::Error> {
         .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
         .body(Body::from(json!({"text": "Test todo"}).to_string()))?;
 
-    let (status, body) = fetch(routers, req).await?;
+    let (status, body) = fetch(&mut routers, req).await?;
     assert_eq!(status, StatusCode::CREATED);
 
     let todo: Todo = serde_json::from_str(&body)?;
