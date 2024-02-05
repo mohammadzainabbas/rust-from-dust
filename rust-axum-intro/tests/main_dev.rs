6ed477d7d585169e4b7a46ad568cad6b7ce50d24
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-05 01:09:01 +0100

    👀 Reviewed and merged code

--- rust-axum-intro/tests/main_dev.rs
+++ rust-axum-intro/tests/main_dev.rs
@@ -1,8 +1,10 @@
-#[allow(unused)]
+#![allow(unused_imports)]
+
 use anyhow::Result;
 use axum::http::response;
 use rust_axum_intro::get_routers;
 use tower::{Service, ServiceExt};
+
 #[tokio::test]
 async fn test_main() -> Result<()> {
     let routers = get_routers().await;
