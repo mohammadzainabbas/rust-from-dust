d1fe036f4165bb7367cae38747e6b543ee1a599f
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-05 01:09:45 +0100

    🔒 Enhanced a module

--- rust-axum-intro/tests/main_dev.rs
+++ rust-axum-intro/tests/main_dev.rs
@@ -1,7 +1,7 @@
 #![allow(unused_imports)]
 
 use anyhow::Result;
-use axum::http::response;
+use axum::http::{response, Request};
 use rust_axum_intro::get_routers;
 use tower::{Service, ServiceExt};
 
@@ -9,7 +9,7 @@ use tower::{Service, ServiceExt};
 async fn test_main() -> Result<()> {
     let routers = get_routers().await;
 
-    let res = routers.oneshot(req)
+    let res = routers.oneshot(Request)
 
     Ok(())
 }
