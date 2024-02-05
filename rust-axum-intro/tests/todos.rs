10d59dd7d10b5c27c4908eb2861a8fbe9f7d61bd
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-05 03:49:37 +0100

    🔒 Enhanced some modules

--- rust-axum-intro/tests/todos.rs
+++ rust-axum-intro/tests/todos.rs
@@ -29,7 +29,7 @@ async fn fetch(
     routers: &mut RouterIntoService<Body>,
     request: Request<Body>,
 ) -> Result<(StatusCode, String), anyhow::Error> {
-    let response = ServiceExt::<Request<Body>>::ready(&mut routers)
+    let response = ServiceExt::<Request<Body>>::ready(routers)
         .await?
         .call(request)
         .await?;
@@ -48,7 +48,7 @@ async fn test_create_todo() -> Result<(), anyhow::Error> {
         .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
         .body(Body::from(json!({"text": "Test todo"}).to_string()))?;
 
-    let (status, body) = fetch(routers, req).await?;
+    let (status, body) = fetch(&mut routers, req).await?;
     assert_eq!(status, StatusCode::CREATED);
 
     let todo: Todo = serde_json::from_str(&body)?;
