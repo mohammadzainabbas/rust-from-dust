dea441ff8ef022381adc3f773d4a93f1eeef0cb7
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-05 03:14:27 +0100

    👨🏻‍💻 Worked on some improvements

--- rust-axum-intro/tests/todos.rs
+++ rust-axum-intro/tests/todos.rs
@@ -61,7 +61,7 @@ async fn test_update_todo() -> Result<(), anyhow::Error> {
 
     let (status, body) = fetch(req).await?;
     assert_eq!(status, StatusCode::CREATED);
-    let created_todo: Todo = serde_json::from_str(&body).unwrap();
+    let created_todo: Todo = serde_json::from_str(&body)?;
     assert_eq!(created_todo.text, "Initial todo");
     assert!(!created_todo.completed);
 
