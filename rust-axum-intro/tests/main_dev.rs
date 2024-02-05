fec6befe01834c6cc56519da8d76ccb29b60f923
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-05 01:10:29 +0100

    📦 Packaged a module

--- rust-axum-intro/tests/main_dev.rs
+++ rust-axum-intro/tests/main_dev.rs
@@ -1,7 +1,7 @@
 #![allow(unused_imports)]
 
 use anyhow::Result;
-use axum::http::{response, Request};
+use axum::{body::Body, http::{response, Request}};
 use rust_axum_intro::get_routers;
 use tower::{Service, ServiceExt};
 
@@ -9,7 +9,7 @@ use tower::{Service, ServiceExt};
 async fn test_main() -> Result<()> {
     let routers = get_routers().await;
 
-    let res = routers.oneshot(Request::builder().uri("/").body(body))
+    let res = routers.oneshot(Request::builder().uri("/").body(Body::empty()))
 
     Ok(())
 }
