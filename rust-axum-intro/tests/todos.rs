3dc4d142d58dfead679f4436c0b5c70d93b93f19
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-05 04:16:47 +0100

    🕵🏻 Conducted code review for new requirement

--- rust-axum-intro/tests/todos.rs
+++ rust-axum-intro/tests/todos.rs
@@ -152,7 +152,7 @@ async fn test_read_todos() -> Result<(), anyhow::Error> {
     assert_eq!(status, StatusCode::OK);
     let created_todos: Vec<Todo> = serde_json::from_str(&body)?;
 
-    assert_eq!(created_todos.len(), n - 1);
+    assert_eq!(created_todos.len(), n);
     assert!(!created_todos.is_empty());
 
     Ok(())
