ee1b1c22727abf651166e91846b4c4e8cdb11a1e
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-05 02:46:06 +0100

    🚀 Deployed an update

--- rust-axum-intro/tests/todos.rs
+++ rust-axum-intro/tests/todos.rs
@@ -20,12 +20,13 @@ use http_body_util::BodyExt; // for `collect`
 use rust_axum_intro::get_routers;
 use tower::{Service, ServiceExt}; // for `call`, `oneshot`, and `ready`
 
-async fn fetch(req: Request<Body>) -> (StatusCode, String) {}
+async fn fetch(req: Request<Body>) -> (StatusCode, String) {
+    let routers = get_routers().await;
+    todo!()
+}
 
 #[tokio::test]
 async fn test_create_todo() -> Result<(), anyhow::Error> {
-    let routers = get_routers().await;
-
     // `Router` implements `tower::Service<Request<Body>>` so we can
     // call it like any tower service, no need to run an HTTP server.
     let res = routers
