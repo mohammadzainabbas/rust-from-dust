7610ac91ee074736660fd852e9b38321ced35f0b
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-05 01:04:51 +0100

    🧱 Laid groundwork for code

--- rust-axum-intro/tests/main_dev.rs
+++ rust-axum-intro/tests/main_dev.rs
@@ -2,7 +2,7 @@
 use anyhow::Result;
 use axum::http::response;
 use rust_axum_intro::get_routers;
-
+use tower::{Service, ServiceExt};
 #[tokio::test]
 async fn test_main() -> Result<()> {
     let routers = get_routers().await;
