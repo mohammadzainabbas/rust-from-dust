b294f353a52d732c34be1860cf51d85c05dbf15f
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-05 01:50:54 +0100

    🔗 Updated external library dependencies

--- rust-axum-intro/tests/hello.rs
+++ rust-axum-intro/tests/hello.rs
@@ -72,7 +72,18 @@ async fn test_say_hello_default() -> Result<()> {
     assert_eq!(&body[..], b"<h3> Hello World! </h3>");
 
     let uris = vec!["/hello", "/hello?names=Mohammad", "/hello?"];
-    uris.iter().for_each(|uri| {});
+    uris.iter().for_each(|uri| {
+        let request = Request::builder().uri(uri).body(Body::empty()).unwrap();
+        let response = ServiceExt::<Request<Body>>::ready(&mut routers)
+            .await
+            .unwrap()
+            .call(request)
+            .await
+            .unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        assert_eq!(&body[..], b"<h3> Hello World! </h3>");
+    });
 
     let uri = "/hello?names=Mohammad";
     let request = Request::builder().uri(uri).body(Body::empty()).unwrap();
