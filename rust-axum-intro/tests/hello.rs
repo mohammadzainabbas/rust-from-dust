e18e4c0cf7b6e31939fb3c9d00b7539cc63d35b3
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-05 01:54:20 +0100

    🌍 Made a new feature

--- rust-axum-intro/tests/hello.rs
+++ rust-axum-intro/tests/hello.rs
@@ -72,7 +72,7 @@ async fn test_say_hello_default() -> Result<()> {
     assert_eq!(&body[..], b"<h3> Hello World! </h3>");
 
     let uris = vec!["/hello", "/hello?names=Mohammad", "/hello?"];
-    uris.iter().for_each(async move |uri| {
+    uris.iter().for_each(move |uri| async {
         let request = Request::builder().uri(uri).body(Body::empty()).unwrap();
         let response = ServiceExt::<Request<Body>>::ready(&mut routers)
             .await
